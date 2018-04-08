aba8e2bb6f5010469d7b1d9d142e904f83c39460
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-08 04:01:03 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,14 +4,15 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include  Memorable::InstanceMethods
+
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
@@ -19,13 +20,6 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
 
   def add_song(song)
     @songs << song
@@ -34,10 +28,6 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
 end
